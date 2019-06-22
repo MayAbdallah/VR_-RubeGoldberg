4cd5c5e1116a936cb6b47b66e20cc6337502f628
MayAbdallah <MayAbdallah@users.noreply.github.com>
2019-06-22 19:52:55 +0300

    -

---
+++ Assets/CheatingWall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class CheatingWall : MonoBehaviour {
+
+    public Throwable th;
+
+    // Use this for initialization
+    void Start()
+    {
+        th = GetComponent<Throwable>();
+
+    }
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Ball") && th.IsAttached())
+        {
+            WaitTime();
+            Destroy(col.gameObject);
+        }
+    }
+
+    IEnumerator WaitTime()
+    {
+        yield return new WaitForSeconds(1); //Wait 1 second then destroy it
+    }
+}
